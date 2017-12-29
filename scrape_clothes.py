01c7ad96d5ab54f153cfe85f4457844649b95f30
timwiesner <timothyjwiesner@gmail.com>
2017-12-28 19:54:33 -0600

    view before product types

---
+++ scrape_clothes.py
@@ -0,0 +1,29 @@
+import requests, json
+
+shopify_sites = {'fashionnova', 'gymshark', 'shoploganpaul', 'donomano'}
+# shopify_sites = {'fashionnova'}
+
+url_template = 'https://www.{}.com/products.json'
+
+for s in shopify_sites:
+    url = url_template.format(s)
+    print(s)
+
+    r = requests.get(url)
+
+    json_data = r.json()
+
+    data = {}
+
+    for each in json_data['products']:
+        title = each['title']
+        product_type = each['product_type']
+        tags = each['tags']
+        data.append({
+            'title': title,
+            'product_type': product_type,
+            'tags': tags
+        })
+
+    result = json.dumps(data)
+    print(result)
