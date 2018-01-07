fd735188330c0ca28fd6c1ab38914bc109c2f75d
timwiesner <timothyjwiesner@gmail.com>
2018-01-07 16:06:26 -0600

    products functions now working

--- mighty_morphin_products.py
+++ mighty_morphin_products.py
@@ -1,91 +1,100 @@
 import requests, json
 
 def get_product_types(site, p_type=1):
-    products = []
-    product_types = []
-    products_by_type = []
-
     url = "https://www.{0}.com/products.json".format(site)
     r = requests.get(url)
     json_data = r.json()
+    products = json_data['products']
+    result = []
 
-    for each in json_data['products']:
-        title = each['title']
-        product_type = each['product_type']
-        products.append({
-            'title': title,
-            'product_type': product_type})
-
-    if p_type == 1:
+    if p_type == 1:  # Return all product types if left empty
         for p in products:
-            product_types.append(p["product_type"])
-        print(sorted(set(product_types)))
-    else:
+            product_type = p['product_type']
+            result.append(product_type)
+    else:  # Return products with specific type
         for p in products:
-            if p_type == p["product_type"]:
-                products_by_type.append(p["title"])
-        print(sorted(set(products_by_type)))
-        # print(products_by_type['title'])
+            if p_type == p['product_type']:
+                title = p['title']
+                result.append(title)
 
+    result = sorted(list(set(result)))
+    result = json.dumps(result, indent=2)
+    print(result)
 
 
 def get_product_tags(site, p_tag=1):
-    products = []
-    product_tags = []
-    products_by_tag = []
-
     url = "https://www.{0}.com/products.json".format(site)
     r = requests.get(url)
     json_data = r.json()
+    products = json_data['products']
+    result = []
 
-    for each in json_data['products']:
-        title = each['title']
-        tags = each['tags']
-        products.append({
-            'title': title,
-            'tags': tags})
-
-    if p_tag == 1:
+    if p_tag == 1:  # Return all tags
         for p in products:
-            for tag in p["tags"]:
-                product_tags.append(tag)
-        print(sorted(set(product_tags)))
-    else:
+            for tag in p['tags']:
+                result.append(tag)
+    else:  # Return products with specific tag
         for p in products:
-            for tag in p["tags"]:
+            for tag in p['tags']:
                 if p_tag == tag:
-                    products_by_tag.append(p["title"])
-        print(sorted(set(products_by_tag)))
+                    title = p['title']
+                    result.append(title)
+
+    result = sorted(list(set(result)))
+    result = json.dumps(result, indent=2)
+    print(result)
 
 
 
 def get_products(site, p_type, p_tag):
-    # 5. With type and tag, return products with matching type and tag
-    products = []
-    products_to_return = []
-
     url = "https://www.{0}.com/products.json".format(site)
     r = requests.get(url)
     json_data = r.json()
-
-    for each in json_data['products']:
-        title = each['title']
-        product_type = each['product_type']
-        tags = each['tags']
-        products.append({
-            'title': title,
-            'product_type': product_type,
-            'tags': tags})
+    products = json_data['products']
+    result = []
 
     for p in products:
-        if p_type == p["product_type"]:
-            products_to_return.append(p["title"])
-        for tag in p["tags"]:
-            if p_tag == tag:
-                products_to_return.append(p["title"])
+        if p_type == p['product_type']:
+            for tag in p['tags']:
+                if p_tag == tag:
+                    title = p['title']
+                    result.append(title)
 
-    print(sorted(set(products_to_return)))
+    result = sorted(list(set(result)))
+    result = json.dumps(result, indent=2)
+    print(result)
 
 
 
-get_product_types(site='fashionnova', p_type='Pants')
+
+# get_product_types(site='gymshark')
+# get_product_types(site='fashionnova')
+# get_product_types(site='teddyfresh')
+# get_product_types(site='gymshark', p_type='T-shirt')
+# get_product_types(site='fashionnova', p_type='Skirts')
+# get_product_tags(site='gymshark')
+# get_product_tags(site='fashionnova')
+# get_product_tags(site='teddyfresh')
+get_product_tags(site='teddyfresh', p_tag='Baseball Cap')
+# get_product_tags(site='gymshark', p_tag='size:s')
+
+# get_products(site='gymshark', p_type='T-shirt', p_tag='size:s')
+
+
+# Reference
+# Companies:
+#     'fashionnova',
+#     'gymshark',
+#     'unifclothing',
+#     'ripndipclothing',
+#     'sisterjane',
+#     'cr7',
+#     'untuckit',
+#     'rebeccaminkoff',
+#     'jackthreads',
+#     'thirdlove',
+#     'teddyfresh',
+#     'teeturtle',
+#     'theyetee',
+#     'onlyny',
+#     'chicnico'
